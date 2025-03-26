495eb8134a1cd26f762648a644e7f0805de5f08e
LindaTancini <lindatancini95@gmail.com>
2025-03-26 15:19:35 +0100

    feat: create array

--- main.js
+++ main.js
@@ -11,6 +11,41 @@ app.get("/", (req, res) => {
   res.send("Server del mio blog");
 });
 
+// CREO L'ARRAY DEI POST
+const posts = [
+  {
+    titolo: "Ciambellone",
+    contenuto: "Dolce perfetto per la colazione.",
+    immagine: "img/ciambellone.jpeg",
+    tag: ["colazione", "dolce", "soffice"],
+  },
+  {
+    titolo: "Cracker alla barbabietola",
+    contenuto: "Snack croccante a base di barbabietola.",
+    immagine: "img/cracker_barbabietola.jpeg",
+    tags: ["snack", "merenda", "barbabietola"],
+  },
+  {
+    titolo: "Pane fritto dolce",
+    contenuto: "Pane fritto, ideale come un dolce veloce.",
+    immagine: "img/pane_fritto_dolce.jpeg",
+    tags: ["dolce", "zucchero", "fritto"],
+  },
+  {
+    titolo: "Pasta alla barbabietola",
+    contenuto: "Un primo piatto molto colorato a base di barbabietola.",
+    immagine: "img/pasta_barbabietola.jpeg",
+    tags: ["pasta", "primo", "barbabietola"],
+  },
+  {
+    titolo: "Torta paesana",
+    contenuto: "Dolce tradizionale paesano a base di pane e cacao.",
+    immagine: "img/torta_paesana.jpeg",
+    tags: ["dolce", "tradizionale", "cacao"],
+  },
+];
+console.log(posts);
+
 // AVVIO IL SERVER SULLA PORTA 3000 E CONTROLLO SU POSTMAN
 app.listen(port, () => {
   console.log(`Sono un server attivo sulla porta:${port}`);
