d4d16a7458411d26d5d27c53947d1616d5c5f7e7
LindaTancini <lindatancini95@gmail.com>
2025-03-26 14:46:43 +0100

    feat: added main route

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const app = express();
 // DEFINISCO LA PORTA
 const port = 3000;
 
+// CREO LA ROTTA PRINCIPALE
+app.get("/", (req, res) => {
+  console.log("Rotta principale");
+  res.send("Server del mio blog");
+});
+
 // AVVIO IL SERVER SULLA PORTA 3000 E CONTROLLO SU POSTMAN
 app.listen(port, () => {
   console.log(`Sono un server attivo sulla porta:${port}`);
